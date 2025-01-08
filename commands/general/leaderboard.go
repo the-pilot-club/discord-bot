c4b659d41e2ea2030ebf04e9299c7018a6858f29
c0d3phr3ak <josh.parkin@gmail.com>
2025-01-07 21:29:11 -0500

    11-points-system - added Leaderboard command.

--- commands/general/leaderboard.go
+++ commands/general/leaderboard.go
@@ -5,5 +5,27 @@ import (
 )
 
 func HandleLeaderboardCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	// Do the command here
+	// Create button component
+	button := discordgo.Button{
+		Label: "TPC Leaderboard",
+		Style: discordgo.LinkButton,
+		URL:   "https://mee6.xyz/thepilotclub",
+	}
+
+	// Create action row with the button
+	actionRow := discordgo.ActionsRow{
+		Components: []discordgo.MessageComponent{button},
+	}
+
+	// Create interaction response with the button
+	response := &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content:    "Check out our leaderboard!",
+			Components: []discordgo.MessageComponent{actionRow},
+		},
+	}
+
+	// Send the response
+	s.InteractionRespond(i.Interaction, response)
 }
