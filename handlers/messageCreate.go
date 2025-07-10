64edbab017275c55020f2c23840c28659adad6a6
vFlyingCowboy <wethepeoplesteiner@gmail.com>
2025-07-10 21:36:06 +0900

    move message response to go routines and have bump wars test in md
    Signed-off-by: vFlyingCowboy <wethepeoplesteiner@gmail.com>

--- handlers/messageCreate.go
+++ handlers/messageCreate.go
@@ -19,52 +19,52 @@ func MessageCreateHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	switch strings.ToLower(m.Content) {
 	case "bump wars":
-		eventresponses.BumpWarsMessage(s, m)
+		go eventresponses.BumpWarsMessage(s, m)
 		return
 	case "what is fno?":
-		eventresponses.FnoMessage(s, m)
+		go eventresponses.FnoMessage(s, m)
 		return
 	case "invite link":
-		eventresponses.InviteLink(s, m)
+		go eventresponses.InviteLink(s, m)
 		return
 	case "invite link mrs bot":
-		eventresponses.InviteLink(s, m)
+		go eventresponses.InviteLink(s, m)
 		return
 	case "moderator":
-		eventresponses.ModeratorMessage(s, m)
+		go eventresponses.ModeratorMessage(s, m)
 		return
 	case "msfs2020 help":
-		eventresponses.Msfs2020Message(s, m)
+		go eventresponses.Msfs2020Message(s, m)
 		return
 	case "rules":
-		eventresponses.RulesMessage(s, m)
+		go eventresponses.RulesMessage(s, m)
 		return
 	case "support":
-		eventresponses.SupportMessage(s, m)
+		go eventresponses.SupportMessage(s, m)
 		return
 	case "tpc callsign":
-		eventresponses.TpcCallsignMessage(s, m)
+		go eventresponses.TpcCallsignMessage(s, m)
 		return
 	case "tpc livery":
-		eventresponses.TpcLiveriesMessage(s, m)
+		go eventresponses.TpcLiveriesMessage(s, m)
 		return
 	case "world tour":
-		eventresponses.WorldTourMessage(s, m)
+		go eventresponses.WorldTourMessage(s, m)
 		return
 	}
 
 	if strings.Contains(strings.ToLower(m.Content), "join vatsim") {
-		eventresponses.JoinVatsimMessage(s, m)
+		go eventresponses.JoinVatsimMessage(s, m)
 	} else if strings.Contains(strings.ToLower(m.Content), "what server") {
-		eventresponses.WhatServerMessage(s, m)
+		go eventresponses.WhatServerMessage(s, m)
 	} else if strings.Contains(strings.ToLower(m.Content), "thanks tpc") {
-		eventresponses.TpcThanksMessage(s, m)
+		go eventresponses.TpcThanksMessage(s, m)
 	} else if strings.Contains(strings.ToLower(m.Content), "what is vatsim?") {
-		eventresponses.WhatIsVatsimMessage(s, m)
+		go eventresponses.WhatIsVatsimMessage(s, m)
 	}
 
 	if m.Type == 8 || m.Type == 9 || m.Type == 10 || m.Type == 11 {
-		eventresponses.BoosterMessageContent(s, m)
+		go eventresponses.BoosterMessageContent(s, m)
 	}
 
 	var Contest = config.GetChannelId(m.GuildID, "Screenshot Contest")
