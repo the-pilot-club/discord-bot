44ac0dd29fc056d1423ec1206d0975fefc3d46a9
vFlyingCowboy <wethepeoplesteiner@gmail.com>
2025-07-16 03:49:28 +0900

    clean up client ready
    Signed-off-by: vFlyingCowboy <wethepeoplesteiner@gmail.com>

--- handlers/clientReady.go
+++ handlers/clientReady.go
@@ -2,16 +2,23 @@ package handlers
 
 import (
 	"github.com/bwmarrin/discordgo"
+	"github.com/getsentry/sentry-go"
 	"log"
 	"tpc-discord-bot/internal/config"
 )
 
 func HandleCLientReady(s *discordgo.Session) {
-	s.UpdateGameStatus(0, "Microsoft Flight Simulator 2024")
+	err := s.UpdateGameStatus(0, "Microsoft Flight Simulator 2024")
+	if err != nil {
+		sentry.CaptureException(err)
+	}
 	log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
 	guilds := s.State.Guilds
 	for _, guild := range guilds {
 		cnl := config.GetChannelId(guild.ID, "Git Channel")
-		s.ChannelMessageSend(cnl, "https://tenor.com/view/b-25-pbj-1j-pbj-commmemorative-air-force-caf-socal-gif-2902325729065148155")
+		_, e := s.ChannelMessageSend(cnl, "https://tenor.com/view/b-25-pbj-1j-pbj-commmemorative-air-force-caf-socal-gif-2902325729065148155")
+		if e != nil {
+			sentry.CaptureException(err)
+		}
 	}
 }
