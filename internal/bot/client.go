5467c6b6e8d631d94a87bbe8ea6ee53c8a63fa5d
vFlyingCowboy <wethepeoplesteiner@gmail.com>
2025-09-23 08:44:53 +0900

    commit staged changes
    Signed-off-by: vFlyingCowboy <wethepeoplesteiner@gmail.com>

--- internal/bot/client.go
+++ internal/bot/client.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"os/signal"
 	"time"
-	cron_jobs "tpc-discord-bot/cron-jobs"
 	"tpc-discord-bot/handlers"
 	"tpc-discord-bot/internal/config"
 	"tpc-discord-bot/util"
@@ -74,7 +73,7 @@ func AddHandlers(s *discordgo.Session) {
 	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
 		go config.IntervalReloadConfigs()
 		go handlers.HandleCLientReady(s)
-		go cron_jobs.HandleCronJobs(s)
+		//go cron_jobs.HandleCronJobs(s)
 	})
 
 	s.AddHandler(func(s *discordgo.Session, m *discordgo.GuildMemberUpdate) {
